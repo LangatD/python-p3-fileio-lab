831de5fd69df643aecbf6f1e2602d345f2f2b48e
LangatD <dorcaschepngetich20@gmail.com>
2024-12-20 13:15:46 +0300

    updated file io

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,23 @@
+import os
 def write_file(file_name, file_content):
-    pass
+    full_file_name = f"{file_name}.txt"
+    
+    with open(full_file_name, "w") as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    full_file_name = f"{file_name}.txt"
+    
+    with open(full_file_name, "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    full_file_name = f"{file_name}.txt"
+    
+    # Check if the file exists
+    if not os.path.exists(full_file_name):
+        return "File does not exist."
+    
+    with open(full_file_name, "r") as file:
+        return file.read()
